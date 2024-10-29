ce663509b69dfff81fcf5aaf0a244061154783d3
devlzg <lzg.lisboasantos5569@gmail.com>
2024-10-29 18:56:40 -0300

    Implementando props funcionais e estéticos

---
+++ src/components/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,8 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript', id: 2, src:livro},
+    {nome: 'Python', id: 3, src:livro},
+    {nome: 'Java', id: 4, src:livro},
+    {nome: 'C', id: 5, src:livro},]
---
+++ src/components/UltimosLancamentos/index.js
@@ -0,0 +1,42 @@
+import { livros } from "./dadosUltimosLancamentos";
+import Titulo from "../Titulo"
+import CardRecomenda from "../CardRecomenda";
+import styled from "styled-components";
+import imagemLivroAngular from "../../imagens/livro2.png"
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
+export default function UltimosLancamentos() {
+    return (
+        <UltimosLancamentosContainer>
+            <Titulo cor="#EB9B00" 
+                    tamanhoFonte="36px" 
+                 >ÚLTIMOS LANÇAMENTOS    
+            </Titulo>
+            <NovosLivrosContainer>
+                { livros.map( livro => (
+                    <img src={livro.src} alt="" />
+                ))}    
+            </NovosLivrosContainer>
+            <CardRecomenda 
+                titulo="Talvez você se interesse: "
+                subtitulo="Angular 11"
+                descricao="Construa apps"
+                img={imagemLivroAngular}
+            />
+        </UltimosLancamentosContainer>
+    )
+}
