47b9073e802c74b7fd5a404e24cd26399f4b3cf1
devlzg <lzg.lisboasantos5569@gmail.com>
2024-10-29 17:38:39 -0300

    Implementando barra de pesquisa com o uso de estados

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input"
 import styled from "styled-components"
 import { useState } from "react"
+import { livros } from "./dadosPesquisa"
 
 const PesquisaContainer = styled.section`
     color: #FFF;
@@ -20,18 +21,47 @@ const Subtitulo = styled.h3`
     font-weight: 500;
     margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 
 export default function Pesquisa () {
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
             <Input 
                 placeholder="Escreva sua próxima leitura"
-                onBlur={evento => setTextoDigitado(evento.target.value)}    
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter( 
+                        livro => livro.nome.includes(textoDigitado)
+                        )
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}    
                 />
-            <p>{textoDigitado}</p>
+                { livrosPesquisados.map( livro => (
+                    <Resultado>
+                        <img src={livro.src} alt="" />
+                        <p>{livro.nome}</p>
+                    </Resultado>
+                )) }
+
         </PesquisaContainer>
     )
 }
